0b84bf1a0842890c0d613bf7bda390ac2556b3e8
Ömer Apaydın <omerapaydin90@gmail.com>
2026-06-04 19:13:59 +0300

    Initial Commit

---
+++ InstagramCloneUI/InstagramCloneUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramCloneUIApp.swift
+//  InstagramCloneUI
+//
+//  Created by Ömer Apaydın on 4.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct InstagramCloneUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
